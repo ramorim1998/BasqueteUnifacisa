3c84f34cb13583795f63c81634ba914243c76e6a
Rafael <araujor920@gmail.com>
2019-07-12 14:52:56 -0300

    mudanças na tela de login

--- login/login.js
+++ login/login.js
@@ -1,14 +1,17 @@
 function entrar(){
-    var nome = document.getElementById("nome");
+   /**  var nome = document.getElementById("nome");
     var email = document.getElementById("email");
     var cpf = document.getElementById("cpf");
     var senha = document.getElementById("senha");
-    var confirme_senha = document.getElementById("confirme-senha");
-
-    if(nome.value!="" && email.value !="" && cpf.value !="" && senha.value !="" && confirme_senha.value == senha.value){
-        window.location = "../loja/loja.html";
+    var confirme_senha = document.getElementById("confirme-senha");*/
+    // validação de informações do usuario para acesso a loja;
+    var infos = [ document.getElementById("nome"),document.getElementById("email"),document.getElementById("cpf")
+,document.getElementById("senha"), document.getElementById("confirme-senha")]
+    if(!infos.includes("")){
+       window.location.href = '../loja/loja.html';
     }else{
-        alert('nome invalido')
+        document.getElementById("feedback").style.color = "red";
+        document.getElementById("feedback").innerHTML = "preencha todos os campos para concluir!"
     }
 
 }
