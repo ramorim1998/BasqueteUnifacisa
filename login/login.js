8e543c61a6f07f49a52ce5c875ecfe910c19aa9b
Rafael <araujor920@gmail.com>
2019-07-15 13:31:22 -0300

    finalizações do projeto

--- login/login.js
+++ login/login.js
@@ -1,21 +1,18 @@
 
 function entrar(){
-   /**  var nome = document.getElementById("nome");
-    var email = document.getElementById("email");
-    var cpf = document.getElementById("cpf");
-    var senha = document.getElementById("senha");
-    var confirme_senha = document.getElementById("confirme-senha");*/
     // validação de informações do usuario para acesso a loja;
    var email = document.getElementById("email");
-    var infos = [ document.getElementById("nome"),document.getElementById("email"),document.getElementById("cpf")
-,document.getElementById("senha"), document.getElementById("confirme-senha")]
-
-    if(!infos.includes("")){
-       window.location.href = '../loja/loja.html';
-    }else{
-        document.getElementById("feedback").style.color = "red";
-        document.getElementById("feedback").innerHTML = "preencha todos os campos para concluir!"
-    }
-
+   var infos = [ document.getElementById("nome").value,document.getElementById("email").value,document.getElementById("cpf").value
+   ,document.getElementById("senha").value, document.getElementById("confirme-senha").value];
+   //verificação se os valores passados foram corretos, para que haja acesso a loja
+       if(!infos.includes("") && infos[3] == infos[4]){
+          window.location.href = '../loja/loja.html';
+         window.open('../loja/loja.html');
+         sessionStorage.setItem("nome","joaquim");
+       }else{
+           document.getElementById("feedback").style.color = "red";
+           document.getElementById("feedback").innerHTML = "preencha todos os campos para concluir!"
+       }
+       
 }
 
--- loja/loja.js
+++ loja/loja.js
@@ -1,6 +1,10 @@
 var carrinho = 0.00;
+//função de compras, adicionando os valores dos produtos ao carrinho
 function compra(value){
     var valor = parseFloat(value);
     carrinho += valor;
     document.getElementById("valor").innerHTML = 'R$ ' + carrinho;
+}
+function nome(){
+    document.getElementById("nome").innerHTML = sessionStorage.getItem("nome");
 }
